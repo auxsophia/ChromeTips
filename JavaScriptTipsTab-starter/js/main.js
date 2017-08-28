c94017ae9f08db27d706332e4d41e98041ae19b9
auxsophia <philosopher.scholar@gmail.com>
2017-08-27 17:45:17 -0700

    Completed.

--- JavaScriptTipsTab-starter/js/main.js
+++ JavaScriptTipsTab-starter/js/main.js
@@ -24,15 +24,24 @@ var tipsList = [
 ];
 
 // Tip Limit counter
-
+var tipLimit = 3;
 
 // Generate a number
-
+function generateNumber() {
+  return Math.floor(Math.random() * tipsList.length);
+}
 
 // Generate a tip:
 // 1. Get random number from generateNumber()
 // 2. Use the random number to get the tip from the array
 // 3. Show the tip
+function generateTip() {
+	var tip = tipsList[generateNumber()];
+	var tipElement = document.querySelector('.js-tip');
+  tipElement.innerHTML = tip;
+	var tipLimitCount = document.querySelector('.tip-limit-count');
+	tipLimitCount.innerHTML = tipLimit;
+}
 
 
 // Tip button click
@@ -42,6 +51,21 @@ var tipsList = [
 // 3a. Subtract 1 from the tipLimit
 // 3b. If the tipLimit is still above or equal to 0, generate a new tip
 // 3c. If not, change the button text and look
+function onTipButtonClick() {
+	var tipButton = document.querySelector('.tip-button');
+	tipButton.addEventListener('click', function() {
+		tipLimit = tipLimit - 1;
+    if (tipLimit >= 0) {
+      generateTip();
 
+      if (0 === tipLimit) {
+        tipButton.innerHTML = 'See you in another tab!';
+        tipButton.classList.add('disabled');
+      }
+    }
+	});
+}
+onTipButtonClick();
 
 // Get the first tip
+generateTip();
